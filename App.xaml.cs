d5258247f9cd7e3ac6f336525ab0c7dc60dbb32f
irisscarlat <iris.scarlat2004@gmail.com>
2025-11-29 15:43:44 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using ScarlatIrisLab7.Data;
+using System.IO;
+
 
 namespace ScarlatIrisLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
+
+            MainPage = new AppShell();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
---
+++ Models/ShopList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ScarlatIrisLab7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
